ea54c826a7678dbf6775503094b0a69b8af9e705
browniemix <62705969+browniemix@users.noreply.github.com>
2020-03-26 13:05:26 -0400

    Create main.tf

---
+++ app-vm-dev-modules/main.tf
@@ -0,0 +1,26 @@
+# Provider Credentials can be loaded via
+# export VSPHERE_SERVER=""
+# export VSPHERE_USER=""
+# export VSPHERE_PASSWORD=""
+provider "vsphere" {
+  allow_unverified_ssl = true
+}
+
+locals {
+  datacenter_name      = "Datacenter"
+  cluster_name         = "East"
+  datastore_name       = "380SSDDatastore2"
+  network_name         = "VM Network"
+  virtual_machine_name = "jB"
+}
+
+module "vm" {
+  source  = "https://tfe-nfcu.digitalinnovation.dev/app/john_b_nfcu1-org/vm/vsphere"
+  version = "0.0.1"
+
+  datacenter_name      = local.datacenter_name
+  cluster_name         = local.cluster_name
+  datastore_name       = local.datastore_name
+  network_name         = local.network_name
+  virtual_machine_name = local.virtual_machine_name
+}
